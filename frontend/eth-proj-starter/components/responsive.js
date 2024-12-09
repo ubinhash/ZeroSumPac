43b8c1f3266e478f387d31f16a9f55f293a749ae
Q <you@example.com>
2024-12-09 13:19:52 -0500

    fix screen too small display bug

--- frontend/eth-proj-starter/components/responsive.js
+++ frontend/eth-proj-starter/components/responsive.js
@@ -24,7 +24,7 @@ const ResponsiveWarning = ({ minWidth = 768 }) => {
         position: "fixed",
         top: 0,
         left: 0,
-        width: "100vw",
+        width: "100%",
         height: "100vh",
         display: "flex",
         justifyContent: "center",
